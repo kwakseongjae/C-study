068d127f74bdb384636085587ac0f071821c415b
kwak <gkffhdnls13@gmail.com>
2021-08-29 15:07:17 +0900

    cal

---
+++ cal2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int add(int a, int b)
+{
+    return a+b;
+}
+int sub(int a, int b)
+{
+    return a-b;
+}
+int mul(int a, int b)
+{
+    return a*b;
+}
+int div(int a, int b)
+{
+    return a/b;
+}
+
+int main(void)
+{
+    int a, b;
+    char sign;
+    printf("Press whole equation\n");
+    scanf("%d%c$d", &a, &sign, &b);
+    if(sign == '+')
+        printf("%d\n", add(a,b));
+}
